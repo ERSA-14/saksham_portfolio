029655803b9ee24913a8c84054bd75d9f748611a
Saksham G <saksham22sg@gmail.com>
2026-05-17 18:28:21 +0000

    minor fix with css

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Build script for GitHub Pages deployment
 # GitHub Pages serves this repo from: https://ersa-14.github.io/static_generator/
-python3 src/main.py /static_generator/
+python3 src/main.py /saksham_portfolio/
